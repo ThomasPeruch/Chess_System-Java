9aa0579ea503d0359b3a4a8110edaec1c3093fb5
ThomasPeruch <peruchthomas@gmail.com>
2020-04-16 12:21:22 -0300

    Check Logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,7 +17,7 @@ public class Program {
         List<ChessPiece> captured = new ArrayList<>();
 
         while(true) {
-            try {
+             try {
                 UI.clearScreen();
                 UI.printMatch(cm,captured);
                 System.out.println();
